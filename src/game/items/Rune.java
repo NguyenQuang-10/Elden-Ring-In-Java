716377fcc0ac22ccfa9ac01b9a86360b77ce8880
Arvind Siva <arvindsiva11@gmail.com>
2023-04-16 15:46:05 +1000

    Add Rune with basic details

---
+++ src/game/items/Rune.java
@@ -0,0 +1,10 @@
+package game.items;
+
+import edu.monash.fit2099.engine.items.Item;
+
+public class Rune extends Item {
+
+    public Rune(int value) {
+        super("Runes", '$', true);
+    }
+}
