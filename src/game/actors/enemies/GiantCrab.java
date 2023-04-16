27b07958f024abbbe106e4ea48b14513851a48fa
Arvind Siva <arvindsiva11@gmail.com>
2023-04-16 15:57:20 +1000

    Add IntrinsicWeapons to enemies

--- src/game/actors/enemies/GiantCrab.java
+++ src/game/actors/enemies/GiantCrab.java
@@ -4,6 +4,7 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 
 public class GiantCrab extends Enemy {
     public GiantCrab() {
@@ -13,5 +14,9 @@ public class GiantCrab extends Enemy {
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         return null;
+    }
+
+    public IntrinsicWeapon getIntrinsicWeapon() {
+        return new IntrinsicWeapon(208, "slams", 90);
     }
 }
--- src/game/actors/enemies/GiantDog.java
+++ src/game/actors/enemies/GiantDog.java
@@ -4,6 +4,7 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 
 public class GiantDog extends Enemy {
     public GiantDog() {
@@ -13,5 +14,9 @@ public class GiantDog extends Enemy {
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         return null;
+    }
+
+    public IntrinsicWeapon getIntrinsicWeapon() {
+        return new IntrinsicWeapon(314, "slams", 90);
     }
 }
