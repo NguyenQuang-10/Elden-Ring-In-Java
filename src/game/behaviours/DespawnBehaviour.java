583e04038e3bc52975b477ad2a03c8e1b44f78a9
Arvind Siva <arvindsiva11@gmail.com>
2023-04-17 13:24:26 +1000

    Add DespawnBehaviour

---
+++ src/game/behaviours/DespawnBehaviour.java
@@ -0,0 +1,27 @@
+package game.behaviours;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actions.AttackAllAction;
+import game.actions.DespawnAction;
+
+import java.util.Random;
+
+public class DespawnBehaviour implements Behaviour{
+
+    private Random rand = new Random();
+    private int despawnProb;
+
+    public DespawnBehaviour(int despawnProb) {
+        this.despawnProb = despawnProb;
+    }
+
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        if (rand.nextInt(100) <= 10) {
+            return new DespawnAction(this.despawnProb);
+        }
+        return null;
+    }
+}
