f693830c4220e26d0adaa5ecf6783c4ad9501bf9
Ryan Nguyen <107928648+flyinRyann@users.noreply.github.com>
2023-04-18 16:36:20 +1000

    Req4 - Implemented Grossmesser and added a method to access it spinning attack action.

---
+++ src/game/weapons/Uchigatana.java
@@ -0,0 +1,32 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+/**
+ * A simple weapon that can be used to attack the enemy.
+ * It deals 115 damage with 80% attack accuracy.
+ * Created by:
+ * @author Ryan Nguyen
+ */
+public class Uchigatana extends WeaponItem {
+
+    /**
+     * Constructor
+     */
+    public Uchigatana() {
+        super("Uchigatana", ')', 115, "", 80);
+    }
+
+    @Override
+    public void tick(Location currentLocation, Actor actor) {}
+
+    /**
+    *
+     */
+    @Override
+    public Action getSkill(Actor target, String direction){
+        return new UnseatheAction();
+    }
+}
