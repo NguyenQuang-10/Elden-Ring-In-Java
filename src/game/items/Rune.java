2a48adaa75aa4e489ffa6a57a040b73c1676ddf0
Arvind Siva <asiv0014@student.monash.edu>
2023-04-28 15:30:59 +1000

    Add Javadoc to Rune

--- src/game/items/Rune.java
+++ src/game/items/Rune.java
@@ -11,26 +11,57 @@ import game.reset.Resettable;
 import game.utils.RandomNumberGenerator;
 import game.utils.Status;
 
+/**
+ * Rune is the currency used by Player for purchases
+ * @author AppliedSession03Gropu03
+ */
 public class Rune extends Item implements Resettable {
 
+    /**
+     * The value of the Rune
+     */
     private int value;
+
+    /**
+     * The location of the Rune if the Rune is on the ground
+     */
     private Location location;
+
+    /**
+     * A public constructor
+     * @param value The value of the Rune
+     */
     public Rune(int value) {
         super("Runes", '$', true);
         this.value = value;
         this.addCapability(Status.RUNE);
     }
 
+    /**
+     * A public constructor used to add Runes to enemies
+     * @param min minimum value of the Rune
+     * @param max maximum value of the Rune
+     */
     public Rune(int min, int max) {
         super("Runes", '$', true);
         this.value = RandomNumberGenerator.getRandomInt(min, max);
         this.addCapability(Status.RUNE);
     }
 
+    /**
+     * Getter for value
+     * @return value of the Rune
+     */
     public int getValue() {
         return this.value;
     }
 
+    /**
+     * Adds Rune to Player if the Player killed enemy and enemy drop the Rune
+     * Drops the Rune to ground if enemy killed by enemy
+     * @param actor the Actor performing the kill
+     * @return DropItemAction if enemy kill enemy else Rune directly added to Player Rune inventory
+     */
     @Override
     public DropAction getDropAction(Actor actor) {
         BuyerSellerList buyerSellerList = BuyerSellerList.getInstance();
@@ -43,6 +74,11 @@ public class Rune extends Item implements Resettable {
         return null;
     }
 
+    /**
+     * Allows the Player to pick up Rune from ground and add to their Rune inventory
+     * @param actor The Actor next to the Rune
+     * @return PickUpAction if it is Player else null
+     */
     @Override
     public PickUpAction getPickUpAction(Actor actor) {
         BuyerSellerList buyerSellerList = BuyerSellerList.getInstance();
@@ -52,10 +88,20 @@ public class Rune extends Item implements Resettable {
         return null;
     }
 
+    /**
+     * Setter for location
+     * @param location the location of the Rune
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Removes Rune on ground from map if Player dies
+     * @param actor The player that triggered game reset
+     * @param map The current gameMap
+     * @return a string representing what happen to reset the Rune
+     */
     @Override
     public String reset(Actor actor, GameMap map) {
         if (!actor.isConscious()) {
