c8f3a5131d471763a89b8e2a681fc61bdd500cc0
Arvind Siva <asiv0014@student.monash.edu>
2023-04-28 15:16:19 +1000

    Add Javadoc to behaviours

--- src/game/behaviours/ReviveBehaviour.java
+++ src/game/behaviours/ReviveBehaviour.java
@@ -7,14 +7,32 @@ import game.actions.ReviveAction;
 import game.actors.enemies.Enemy;
 import game.actors.enemies.Reviver;
 
+/**
+ * A behaviour that determines whether Reviver should revive the Actor that spawned it
+ * @author AppliedSession03Gropu03
+ */
 public class ReviveBehaviour implements Behaviour{
 
+    /**
+     * The Actor that will revive the Actor that Spawned it
+     */
     private Reviver reviver;
 
+    /**
+     * A public constructor
+     * @param reviver The Actor that will revive the Actor that Spawned it
+     */
     public ReviveBehaviour(Reviver reviver){
         this.reviver = reviver;
     };
 
+    /**
+     * Decides whether to revive the Actor that spawned the Reviver or not based on
+     * condition set by the Reviver
+     * @param actor the Actor acting
+     * @param map the GameMap containing the Actor
+     * @return ReviveAction or null
+     */
     @Override
     public Action getAction(Actor actor, GameMap map) {
         if (this.reviver.toRevive()) {
--- src/game/behaviours/SpawnBehaviour.java
+++ src/game/behaviours/SpawnBehaviour.java
@@ -6,13 +6,31 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import game.actions.SpawnAction;
 import game.actors.enemies.Enemy;
 
+/**
+ * A behaviour that determines whether an enemy should spawn an enemy or not
+ * @author AppliedSession03Gropu03
+ */
 public class SpawnBehaviour implements Behaviour {
+
+    /**
+     * The enemy to be spawned
+     */
     private Enemy toSpawn;
 
+    /**
+     * A public constructor
+     * @param toSpawn The enemy to be spawned
+     */
     public SpawnBehaviour(Enemy toSpawn) {
         this.toSpawn = toSpawn;
     }
 
+    /**
+     * Spawns the enemy to be spawned if the Actor is dead
+     * @param actor the Actor acting
+     * @param map the GameMap containing the Actor
+     * @return SpawnAction or null
+     */
     @Override
     public Action getAction(Actor actor, GameMap map) {
         if (!actor.isConscious()) {
