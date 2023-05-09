a8fd2a2f40929a38d0569e74681fa630020d6a6c
Nhat Quang Nguyen <nngu0112@student.monash.edu>
2023-05-09 19:26:02 +1000

    Added GoldenFogDoor, added a sample use case in Application where user moves to Roundtable Holds

--- src/game/Application.java
+++ src/game/Application.java
@@ -13,6 +13,7 @@ import game.environments.*;
 import game.items.GoldenRune;
 import game.utils.ArchetypeManager;
 import game.utils.FancyMessage;
+import game.utils.Maps;
 import game.weapons.RemembranceOfTheGrafted;
 
 /**
@@ -28,7 +29,9 @@ public class Application {
 
 		World world = new World(new Display());
 
-		FancyGroundFactory groundFactory = new FancyGroundFactory(new SiteOfLostGrace() ,new Dirt(), new Wall(), new Floor(), new Graveyard(), new PuddleOfWater(), new GustOfWind(), new Cliff());
+		FancyGroundFactory groundFactory = new FancyGroundFactory(new SiteOfLostGrace() ,new Dirt(),
+				new Wall(), new Floor(), new Graveyard(), new PuddleOfWater(), new GustOfWind(), new Cliff()
+				, new Cage(), new Barrack());
 
 		List<String> map = Arrays.asList(
 				"...........................................................................",
@@ -56,7 +59,9 @@ public class Application {
 				"..............................................................###..__###...",
 				"...........................................................................");
 		GameMap gameMap = new GameMap(groundFactory, map);
+		GameMap roundtableHold = new GameMap(groundFactory, Maps.ROUNDTABLE_HOLD);
 		world.addGameMap(gameMap);
+		world.addGameMap(roundtableHold);
 
 		// BEHOLD, ELDEN RING
 		for (String line : FancyMessage.ELDEN_RING.split("\n")) {
@@ -82,6 +87,8 @@ public class Application {
 		// Player player = new Player("Tarnished", '@', 300);
 		world.addPlayer(player, gameMap.at(36, 10));
 		player.setLastSiteOfLostGrace(gameMap.at(36, 10));
+
+		gameMap.at(30,9).setGround(new GoldenFogDoor(roundtableHold.at(10, 6), "to Roundtable Hold"));
 
 		world.run();
 	}
