eceae72f9ba682806febd732fb6538b5c975bfd0
Arvind Siva <asiv0014@student.monash.edu>
2023-05-11 16:52:38 +1000

    Add SummonSign and SummonAction

---
+++ src/game/environments/SummonSign.java
@@ -0,0 +1,26 @@
+package game.environments;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.SummonAction;
+import game.utils.Status;
+
+public class SummonSign extends Ground {
+    /**
+     * Constructor.
+     */
+    public SummonSign() {
+        super('=');
+    }
+
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
+        ActionList actions = super.allowableActions(actor, location, direction);
+        if (actor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+            actions.add(new SummonAction());
+        }
+        return actions;
+    }
+}
