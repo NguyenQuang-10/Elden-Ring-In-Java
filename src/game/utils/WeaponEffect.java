34e6e9bdd13f8410a45bd3c99cc335242c8814a9
Arvind Siva <asiv0014@student.monash.edu>
2023-05-16 16:26:02 +1000

    Add Javadoc

--- src/game/utils/WeaponEffect.java
+++ src/game/utils/WeaponEffect.java
@@ -3,7 +3,7 @@ package game.utils;
 /**
  *  Enum class for adding effects to Actors who are attacked using weapons with
  *  effects that lasts for 3 turns
- *  Modified by: AppliedSession03Group03
+ *  @author AppliedSession03Group03
  */
 public enum WeaponEffect {
     POISONED,
