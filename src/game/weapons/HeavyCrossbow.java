9d20b04235ba10815a56d9047943cf3698435928
Arvind Siva <asiv0014@student.monash.edu>
2023-05-17 17:34:28 +1000

    Add Javadoc

--- src/game/weapons/HeavyCrossbow.java
+++ src/game/weapons/HeavyCrossbow.java
@@ -17,16 +17,30 @@ public class HeavyCrossbow extends WeaponItem implements PurchaseableWeapon, Sel
         super("HeavyCrossbow", '}', 64, "shoots", 57);
     }
 
+    /**
+     * Getter to get the purchase price of the item.
+     * @return The purchase price of the item.
+     * @see PurchaseableWeapon
+     */
     @Override
     public int getPurchasePrice() {
         return 1500;
     }
 
+    /** Instantiate a new instance of the weapon and return it.
+     * @return New instance of the club.
+     * @see PurchaseableWeapon
+     */
     @Override
     public WeaponItem purchaseItem() {
         return new HeavyCrossbow();
     }
 
+    /**
+     * Getter to get the sell price of the item.
+     * @return The sell price of the item.
+     * @see SellableWeapon
+     */
     @Override
     public int getSellPrice() {
         return 100;
