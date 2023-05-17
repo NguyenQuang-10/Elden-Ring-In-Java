cde12ec4a427582e475889f7b0389e11584db6bb
Arvind Siva <asiv0014@student.monash.edu>
2023-05-17 21:42:06 +1000

    Add Javadoc to ExchangeAbleItem

--- src/game/actions/traderactions/ExchangeItemAction.java
+++ src/game/actions/traderactions/ExchangeItemAction.java
@@ -6,17 +6,39 @@ import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.WeaponItem;
 
+/**
+ * An action used by Player to exchange an Item for a Weapon with Trader
+ * @author AppliedSession03Group03
+ */
 public class ExchangeItemAction extends Action {
 
+    /**
+     * The Player's item to be exchanged for a weapon
+     */
     private Item actorItem;
+
+    /**
+     * The weapon to be received by Player in the exchange
+     */
     private WeaponItem returnWeapon;
 
+    /**
+     * A public constructor
+     * @param actorItem The Player's item to be exchanged for a weapon
+     * @param returnWeapon The weapon to be received by Player in the exchange
+     */
     public ExchangeItemAction(Item actorItem, WeaponItem returnWeapon) {
         this.actorItem = actorItem;
         this.returnWeapon = returnWeapon;
     }
 
-
+    /**
+     * Remove the item to exchange from the Player and add the weapon to the Player
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return String representing the status of the exchange
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         actor.removeItemFromInventory(this.actorItem);
@@ -25,6 +47,12 @@ public class ExchangeItemAction extends Action {
         return this.actorItem.toString() + " has been exchanged for " + this.returnWeapon.toString();
     }
 
+    /**
+     * Returns the menu descriptor of the action
+     *
+     * @param actor The actor performing the action.
+     * @return The menu description of the action
+     */
     @Override
     public String menuDescription(Actor actor) {
         return "Exchange " + this.actorItem.toString() + " for " + this.returnWeapon.toString();
--- src/game/items/ExchangeableItem.java
+++ src/game/items/ExchangeableItem.java
@@ -13,6 +13,14 @@ import game.weapons.GraftedDragon;
  * @author AppliedSession03Group03
  */
 public interface ExchangeableItem {
+
+    /**
+     * Returns a list of actions that allow the Player to exchange the ExchangeableItem
+     * for a Weapon
+     *
+     * @param actor The Actor performing the exchange
+     * @return a list of actions that allow the Player to exchange the ExchangeableItem for a Weapon
+     */
     default ActionList getExchangeWeaponAction(Actor actor){
         ActionList actions = new ActionList();
         for (Item item: actor.getItemInventory()) {
