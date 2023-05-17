9b82d9ca45cf9d03d038d2701d9cb9bfc5875571
Arvind Siva <asiv0014@student.monash.edu>
2023-05-17 17:24:03 +1000

    Add Javadoc

--- src/game/environments/Barrack.java
+++ src/game/environments/Barrack.java
@@ -6,11 +6,23 @@ import game.actors.enemies.Dog;
 import game.actors.enemies.GodrickSoldier;
 import game.utils.RandomNumberGenerator;
 
+/**
+ * A class that represents Barrack ground
+ * @author AppliedSession03Group03
+ */
 public class Barrack extends Ground {
+
+    /**
+     * A public constructor
+     */
     public Barrack() {
         super('B');
     }
 
+    /**
+     * At every turn Cage spawns a GodrickSoldier enemy with 45% probability
+     * @param location The location of the Ground
+     */
     @Override
     public void tick(Location location) {
         int rand = RandomNumberGenerator.getRandomInt(0, 100);
--- src/game/environments/Cage.java
+++ src/game/environments/Cage.java
@@ -8,11 +8,23 @@ import game.actors.enemies.SkeletalBandit;
 import game.utils.Calculation;
 import game.utils.RandomNumberGenerator;
 
+/**
+ * A class that represents cage ground
+ * @author AppliedSession03Group03
+ */
 public class Cage extends Ground {
+
+    /**
+     * A public constructor
+     */
     public Cage() {
         super('<');
     }
 
+    /**
+     * At every turn Cage spawns a Dog enemy with 37% probability
+     * @param location The location of the Ground
+     */
     @Override
     public void tick(Location location) {
         int rand = RandomNumberGenerator.getRandomInt(0, 100);
